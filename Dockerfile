f2e902d2dcc2d6d595ca183e56536def403beafb
emreeren <emre.eren@emlakjet.com>
2022-12-18 18:40:16 +0300

    + fe application added. + cors problem fixed. + application.yml for production added. + applications dockerized. + first releases done.

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM maven:3.8.4-openjdk-17-slim as build
+WORKDIR /app
+COPY . .
+# RUN mvn -f /app/pom.xml -B --no-transfer-progress dependency:go-offline
+RUN --mount=type=cache,target=/root/.m2,rw mvn -f /app/pom.xml -B -U clean package # -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
+
+
+FROM openjdk:17-jdk-slim
+WORKDIR /app
+
+COPY --from=build /app/codexist-infra/target/codexist-infra*.jar /codexist-infra.jar
+
+ENV TZ Europe/Istanbul
+
+EXPOSE 9090
+
+ENTRYPOINT ["java", "-XX:+UseG1GC", "-XX:+UseStringDeduplication", "-jar", "-Dspring.profiles.active=prod", "/codexist-infra.jar"]
--- codexist-infra/src/main/java/dev/remreren/adapter/location/rest/PlacesNearLocationRestController.java
+++ codexist-infra/src/main/java/dev/remreren/adapter/location/rest/PlacesNearLocationRestController.java
@@ -5,10 +5,7 @@ import dev.remreren.location.model.PlaceResponse;
 import dev.remreren.location.usecase.GetPlacesNearLocationUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +15,7 @@ public class PlacesNearLocationRestController {
     private final UseCaseHandler<PlaceResponse, GetPlacesNearLocationUseCase> getPlacesNearUseCaseHandler;
 
     @GetMapping
+    @CrossOrigin
     public ResponseEntity<PlaceResponse> getPlace(@RequestParam("lat") Double lat,
                                                   @RequestParam("lng") Double lng,
                                                   @RequestParam("radius") Long radius) {
